bc7b408a2535f5b91f43de6e19c9fe888636f9dc
Cphayim <admin@cphayim.me>
2017-08-17 09:29:23 +0800

    修复了一个可能导致 loading 和 toast 重叠的 bug
    修复了一个可能导致 loading 和 toast 重叠的 bug

--- src/modules/toast.ts
+++ src/modules/toast.ts
@@ -3,7 +3,7 @@
  * @Author: Cphayim
  * @Date: 2017-07-26 09:44:52 
  * @Last Modified by: Cphayim
- * @Last Modified time: 2017-08-14 01:11:16
+ * @Last Modified time: 2017-08-16 14:25:36
  */
 
 class Toast {
@@ -15,10 +15,11 @@ class Toast {
      * @returns 返回 promise 对象
      */
     public showLoading(title: string = '', mask: boolean = false) {
-        return new Promise((resolve,reject)=>{
+        this.hide();
+        return new Promise((resolve, reject) => {
             wx.showLoading({
-                title,mask,
-                success(res){
+                title, mask,
+                success(res) {
                     resolve(res);
                 },
                 fail(err) {
@@ -71,10 +72,11 @@ class Toast {
      * @param {Object} 解构对象 {title='',duration=10000,mask=false,icon="success",image=''} 
      * @returns 返回 promise 对象 
      */
-    private show({ title = '', duration = 10000, mask = false, icon = "success", image = '' }: any) {
+    private show({ title = '', duration = 10000, mask = false, icon = "none", image = '' }: any) {
+        this.hide();
         return new Promise((resolve, reject) => {
             wx.showToast({
-                title,icon,image,duration,mask,
+                title, icon, image, duration, mask,
                 success(res) {
                     resolve(res);
                 },
@@ -86,12 +88,9 @@ class Toast {
     }
     /**
      * 隐藏 toast
-     * @param {number} [dalay=0] 延迟时间，毫秒，默认
      */
-    public hide(delay = 0) {
-        setTimeout(() => {
-            wx.hideToast();
-        }, delay);
+    public hide() {
+        wx.hideToast();
     }
 }
 
