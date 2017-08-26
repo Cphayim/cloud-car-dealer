633d9df8d493af5c9618cd70b292b03784406287
Cphayim <admin@cphayim.me>
2017-08-27 02:23:25 +0800

    待建档客户列表 静态完成

--- src/pages/detail/record/record-list/record-list.ts
+++ src/pages/detail/record/record-list/record-list.ts
@@ -1,7 +1,32 @@
+/*
+ * 待建档客户列表
+ * @Author: 云程科技
+ * @Date: 2017-08-27 02:19:42 
+ * @Last Modified by: Cphayim
+ * @Last Modified time: 2017-08-27 02:22:45
+ */
+
 import BasePage from '../../../basepage';
+import pagePath from '../../../../config/path.config';
 
+interface Data {
+    loaded: boolean;
+}
 class RecordListPage extends BasePage {
-
+    public data: Data = {
+        loaded: false,
+    }
+    private onLoad(options) {
+        this.setData({
+            loaded: true
+        });
+    }
+    private itemToEnter(e) {
+        const id: string = e.currentTarget.dataset.id;
+        wx.navigateTo({
+            url: `${pagePath['record-info']}?id=${id}`
+        });
+    }
 }
 
 Page(new RecordListPage());
