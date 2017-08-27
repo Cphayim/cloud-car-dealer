5b4abde731fd566d11c5d75b54b2cd7a18c8d784
Cphayim <admin@cphayim.me>
2017-08-27 15:41:15 +0800

    待建档客户线索详情 静态完成

--- src/pages/detail/record/record-info/record-info.ts
+++ src/pages/detail/record/record-info/record-info.ts
@@ -1,7 +1,49 @@
+/*
+ * 待建档客户 线索详情
+ * @Author: Cphayim 
+ * @Date: 2017-08-27 02:40:05 
+ * @Last Modified by: Cphayim
+ * @Last Modified time: 2017-08-27 15:39:25
+ */
+
 import BasePage from '../../../basepage';
+import pagePath from '../../../../config/path.config';
+
+interface Data {
+    loaded: boolean;
+    introData: {
+        avatar: string,
+        realname: string,
+        username: string,
+        time?: string,
+        phone?: string,
+        customerId: number,
+    }
+}
 
 class RecordInfoPage extends BasePage {
-
+    private id: number = 0;
+    public data: Data = {
+        loaded: false,
+        introData: {
+            customerId: 100,
+            avatar: 'https://cphayim.me/wp-content/uploads/2017/05/cphayim-avatar-2017.jpg',
+            realname: 'Cphayim',
+            username: 'Cphayim',
+        }
+    }
+    private onLoad(options) {
+        const id: number = ~~options.id;
+        this.id = id;
+        this.setData({
+            loaded: true
+        });
+    }
+    private goToRecord() {
+        wx.navigateTo({
+            url: `${pagePath['record-edit']}?id=${this.id}`
+        });
+    }
 }
 
 Page(new RecordInfoPage());
