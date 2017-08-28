178cc0f337b1c2406b5531035b34f4f49e64ec51
Cphayim <admin@cphayim.me>
2017-08-28 18:15:51 +0800

    edit: tab客户整页结构与样式修改
    tab客户整页结构与样式修改
    保持与 tab 业务一致:
    1.列表使用 list 组件的通用样式
    2.顶部组件使用置顶效果

--- src/pages/tabs/client/client.ts
+++ src/pages/tabs/client/client.ts
@@ -5,35 +5,16 @@ import toast from '../../../modules/toast';
 import { domain } from '../../../config/url.config';
 import { openSearch } from '../../../components/search/search';
 import { request } from '../../../modules/request';
+import { enumConfig } from '../../../config/enum.config';
 
 /*
  * Tab 客户页逻辑
  * @Author: 云程科技 
  * @Date: 2017-06-30 10:12:17 
  * @Last Modified by: Cphayim
- * @Last Modified time: 2017-08-14 10:13:05
+ * @Last Modified time: 2017-08-28 15:10:47
  */
 
-// 测试数据
-const clientData = [];
-
-const levelArr = ['H', 'A', 'B', 'C'];
-for (let i = 0; i < levelArr.length; i++) {
-    const listObj: any = {};
-    const clientList = [];
-    listObj.level = levelArr[i];
-    listObj.clientList = clientList;
-    for (let j = 0; j < 10; j++) {
-        const clientObj: any = {}
-        clientObj.realname = clientObj.username = `第${j + 1}位${levelArr[i]}级客户`;
-        clientObj.signature = `客户签名`.repeat(j + 1);
-        clientObj.avatarUrl = 'https://cphayim.me/wp-content/uploads/2017/05/cphayim-avatar-2017.jpg';
-        clientObj.id = ~~(Math.random() * 10000);
-        clientObj.time = '19:30';
-        clientList.push(clientObj);
-    }
-    clientData.push(listObj);
-}
 
 // 实例化 Selection 组件类
 const selectionDataJSON = `[{"name":"orderby","title":"排列","type":"select","opts":[{"title":"绑定顾问时间","val":"0"},{"title":"关注时间","val":"1"},{"title":"最近活跃时间","val":"2"}]},{"name":"channel","title":"沟通渠道","type":"screening","opts":[{"title":"全部","val":"10"},{"title":"电话","val":"20"},{"title":"微信","val":"30"}]},{"name":"level","title":"等级","type":"all","opts":[{"title":"全部","val":"all"},{"title":"H","val":"h"},{"title":"A","val":"a"},{"title":"B","val":"b"},{"title":"C","val":"c"}]},{"name":"carModel","title":"车系","type":"screening","opts":[{"title":"一类车","val":"10"},{"title":"二类车","val":"20"},{"title":"三类车","val":"30"}]},{"name":"binding","title":"绑定时间","type":"all","opts":[{"title":"全部","val":"10"},{"title":"今天","val":"20"},{"title":"昨天","val":"30"},{"title":"近7日","val":"40"},{"title":"本月","val":"50"}]},{"name":"pretime","title":"最近联系时间","type":"screening","opts":[{"title":"全部","val":"10"},{"title":"今天","val":"20"},{"title":"昨天","val":"30"},{"title":"近7日","val":"40"},{"title":"本月","val":"50"}]}]`;
@@ -42,25 +23,46 @@ const selection = new Selection({
 });
 
 class ClientPage extends BasePage {
-    private selection = selection;
-    private sideTimer: number = 0; // 侧栏导航显示计时器
+    private selection: Selection = selection;
+
+    // 视图滚动属性
+    private listPaddingTop: number = 0;
+    private selectionHeight: number = 0;
+    private searchHeight: number = 0;
+
+
     private data: any = {
-        pagePath,
-        // 客户列表滚动区 scrollView 高度
-        clientScrollViewHeight: 0, // 页面首次渲染时更新
+        pagePath: pagePath,
+        // 列表顶部填充层高度
+        listPaddingTop: this.listPaddingTop,
         // selection 对象挂载
-        // 客户列表数据
-        clientData: clientData,
-        // 侧边控制导航属性
-        // 客户列表是否正在滚动，滚动时显示侧边导航
-        scrolling: false,
+        selection: this.selection,
+
+        /**
+         * 请求相关
+         */
+
+
+        pageSize: 20, // 分页每页数量，用于上拉加载
+        pageNo: 0, // 当前页码
+        listCount: 0, // 当前列表项数
+        orderBy: 'CreateTime desc,Status asc',
+        PreFrom: '',
+        Status: '',
+
+        /*
+         * 枚举相关
+         */
+        OpportunityPreFrom: enumConfig.OpportunityPreFrom,
+        OpportunityStatus: enumConfig.OpportunityPreStatus,
+        /**
+         * 数据
+         */
+        clientTotal: 0, // 客户总数
+        clientList: [], // 客户列表
     }
     // 搜索组件 入口方法挂载
     public openSearch = openSearch;
-    constructor() {
-        super();
-        this.data.selection = selection;
-    }
     /**
      * 展开下拉选项卡
      * 触发条件：Select tap 事件
@@ -130,86 +132,71 @@ class ClientPage extends BasePage {
         this.selection.resetQuery();
     }
     /**
-     * 客户列表滚动事件
+     * 添加列表数据
+     * @param {boolean} isReset 是否重置数据列表
+     * @param {boolean} isRefresh 是否来自下拉刷新
+     * @returns 
      */
-    public clientListScroll(e) {
-        // 清空计时器
-        clearTimeout(this.sideTimer);
-        this.sideTimer = 0;
-        this.setData({
-            scrolling: true
-        });
-        // 设置延迟
-        this.sideTimer = setTimeout(() => {
-            this.setData({
-                scrolling: false
-            });
-        }, 2000);
-    }
-    /**
-     * 滚动到指定的列表(等级)
-     */
-    public scrollToList(e) {
-        console.log(e);
-        let id = e.target.dataset.levelId || '';
-        console.log(id);
-        this.setData({
-            scrollToId: id
-        });
-        console.log(this.data.scrollToId);
+    private addListItem(isReset = false, isRefresh = false){
+        let msg = isRefresh ? '正在刷新' : '正在加载';
+        toast.showLoading(msg, true);
+        // 如果是刷新重置部分请求配置
+        if (isReset) {
+            this.data.pageNo = 0;
+            this.data.listCount = 0;
+            this.data.clientList = [];
+        }
+        // 页数自增
+        this.data.pageNo++;
+        return this.requestList({});
     }
 
+    private requestList({
+        
+    }){
+
+    }
     /**
      * 生命周期函数--监听页面加载
      */
     private onLoad(options) {
-        request({
-            url:`${domain}/UC/CustomerPre/ReadForSearch`,
-            data:{
-                ticket: '0c939c84-754d-4e9f-93cc-61ccece08d9d',
-                PageSize:20,
-                PageNo: 2
-            }
-        }).then(res=>{
-            console.log(res);
-        }).catch(err=>console.log(err));
         // 页面加载时调用 selection 的 init 方法初始化组件
         this.selection.init();
-    }
 
-    /**
-     * 监听页面初次渲染完成
-     */
-    private onReady() {
-        // 更新 m-client 的 scrollView 容器高度
-        // 拿到 .m-client-scroll-view 距离视口顶部的高度
-        wx.createSelectorQuery().select('.m-client-scroll-view')
-            // 查询字段
-            .fields({
-                size: true, // 元素大小
-                rect: true // 元素位置
-            }, (res) => {
-                this.setData({
-                    clientScrollViewHeight: wx.getSystemInfoSync().windowHeight - res.top
-                });
+        // 获取 search 入口组件高度
+        let p1 = new Promise((resolve, reject) => {
+            wx.createSelectorQuery().select('.m-search-outer').boundingClientRect(rect => {
+                resolve(rect.height);
             }).exec();
-    }
-    private onShow() {
+        });
+        // 获取 selection 组件高度
+        let p2 = new Promise((resolve, reject) => {
+            wx.createSelectorQuery().select('.m-options-box').boundingClientRect(rect => {
+                resolve(rect.height);
+            }).exec();
+        });
+        // 设置 listPaddingTop
+        Promise.all([p1, p2]).then((resAll: number[]) => {
+            this.searchHeight = resAll[0];
+            this.selectionHeight = resAll[1];
+            this.listPaddingTop = this.searchHeight + this.selectionHeight;
+            this.setData({
+                listPaddingTop: this.listPaddingTop,
+            });
+        });
 
+        // 初始化添加一次数据
+        this.addListItem();
     }
-    private onHide() {
 
+    private onReady() {
     }
-    private onUnload() {
-
-    }
-    private onPullDownRefresh() {
-    }
-    private onReachBottom() {
-    }
-    private onShareAppMessage() {
-
-    }
+    private onShow() { }
+    private onHide() { }
+    private onUnload() { }
+    private onPullDownRefresh() { }
+    private onReachBottom() { }
+    private onShareAppMessage() { }
 }
 
 Page(new ClientPage());
