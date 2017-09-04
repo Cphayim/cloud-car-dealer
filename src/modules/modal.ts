94ae3250499e426056b84f5fb8ee52c78ead1456
Cphayim <admin@cphayim.me>
2017-09-04 15:03:17 +0800

    U: 更新了部分默认值

--- src/modules/modal.ts
+++ src/modules/modal.ts
@@ -5,30 +5,31 @@
  * @Last Modified by: Cphayim
  * @Last Modified time: 2017-08-10 22:37:14
  */
-
- export default {
-     show({
-         title= '提示',
-         content = '提示内容',
-         showCancel = true,
-         cancelText = '取消',
-         cancelColor = '#333',
-         confirmText = '确定',
-         confirmColor = '#54b4ef',
-
-     }){
-        return new Promise((resolve,reject)=>{
+export const modal = {
+    show({
+        title = '提示',
+        content = '提示内容',
+        showCancel = true,
+        cancelText = '取消',
+        cancelColor = '#333',
+        confirmText = '确定',
+        confirmColor = '#54b4ef'
+     }) {
+        return new Promise((resolve, reject) => {
             wx.showModal({
-                title,content,showCancel,cancelText,cancelColor,confirmText,confirmColor,
-                success(res){
-                    resolve(res);
+                title, content, showCancel, cancelText, cancelColor, confirmText, confirmColor,
+                success(res) {
+                    if (res.confirm) {
+                        resolve(true);
+                    } else if (res.cancel) {
+                        resolve(false);
+                    }
                 },
-                fail(err){
+                fail(err) {
                     console.log(err);
-                    reject(err);
                 }
             });
         });
-         
-     }
- }
+
+    }
+}
