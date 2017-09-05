3c330108e084d7adf98e0dea06c50b08650d307f
Cphayim <admin@cphayim.me>
2017-09-05 11:40:15 +0800

    A: 添加客户成交信息页

--- src/config/path.config.ts
+++ src/config/path.config.ts
@@ -65,6 +65,7 @@ const pathArr: Path[] = [
     { name: 'customer-edit', pre: 'detail/customer' },
     // 客户订单信息
     { name: 'customer-order', pre: 'detail/customer' },
+    // 客户成交信息
     { name: 'customer-deal', pre: 'detail/customer' },
     // 客户流失登记(三合一)
     { name: 'customer-lose', pre: 'detail/customer' },
