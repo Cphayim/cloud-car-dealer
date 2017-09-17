2b6c03a3caf277884b2f8ad8f4c3d86a05c40a3f
Cphayim <admin@cphayim.me>
2017-09-17 23:28:52 +0800

    A: toast 模块添加三个方法

--- src/modules/toast.ts
+++ src/modules/toast.ts
@@ -6,7 +6,16 @@
  * @Last Modified time: 2017-08-29 11:10:41
  */
 
+const path = '/assets/toast-icons';
+
 class Toast {
+    private iconPath: string;
+    constructor(path: string = '/assets/toast-icons') {
+        if (!/\/$/.test(path)) {
+            path += '/';
+        }
+        this.iconPath = path;
+    }
     /**
      * 显示 loadding(等待) toast
      * @param {string} [title=''] 显示的文本
@@ -22,7 +31,7 @@ class Toast {
                     resolve(res);
                 },
                 fail(err) {
-                    reject();
+                    reject(err);
                 }
             });
         })
@@ -30,12 +39,12 @@ class Toast {
     /**
      * 显示 success(成功) toast
      * @param {string} [title=''] 显示的文本
-     * @param {number} [duration=10000] 持续时间 
+     * @param {number} [duration=2000] 持续时间 
      * @param {boolean} [mask=false] 是否遮罩(透明层)
      * @returns 返回 promise 对象
      */
     public showSuccess(title: string = '', duration: number = 2000, mask: boolean = false) {
-        const image: string = '/assets/toast-icons/toast-success.png';
+        const image: string = this.iconPath + 'toast-success.png';
         return this.show({
             title, duration, mask, image
         });
@@ -43,7 +52,7 @@ class Toast {
     /**
      * 显示 warning(警告) toast
      * @param {string} [title=''] 显示的文本
-     * @param {number} [duration=10000] 持续时间 
+     * @param {number} [duration=2000] 持续时间 
      * @param {boolean} [mask=false] 是否遮罩(透明层)
      * @returns 返回 promise 对象
      */
@@ -56,12 +65,51 @@ class Toast {
     /**
      * 显示 error(错误) toast
      * @param {string} [title=''] 显示的文本
-     * @param {number} [duration=10000] 持续时间 
+     * @param {number} [duration=4000] 持续时间 
      * @param {boolean} [mask=false] 是否遮罩(透明层)
      * @returns 返回 promise 对象
      */
     public showError(title: string = '', duration: number = 4000, mask: boolean = false) {
         const image: string = '/assets/toast-icons/toast-error.png';
+        return this.show({
+            title, duration, mask, image
+        });
+    }
+    /**
+     * 显示 voiced(有声的) toast
+     * @param {string} [title=''] 显示的文本
+     * @param {number} [duration=2000] 持续时间 
+     * @param {boolean} [mask=false] 是否遮罩(透明层)
+     * @returns 返回 promise 对象
+     */
+    public showVoiced(title: string = '', duration: number = 2000, mask: boolean = false) {
+        const image: string = '/assets/toast-icons/toast-voiced.png';
+        return this.show({
+            title, duration, mask, image
+        });
+    }
+    /**
+     * 显示 voicing(正在录音) toast
+     * @param {string} [title=''] 显示的文本
+     * @param {number} [duration=10m] 持续时间 
+     * @param {boolean} [mask=false] 是否遮罩(透明层)
+     * @returns 返回 promise 对象
+     */
+    public showVoicing(title: string = '', duration: number = 10*60*1000, mask: boolean = false) {
+        const image: string = '/assets/toast-icons/toast-voicing.gif';
+        return this.show({
+            title, duration, mask, image
+        });
+    }
+    /**
+     * 显示 cancel(取消) toast
+     * @param {string} [title=''] 显示的文本
+     * @param {number} [duration=2000] 持续时间 
+     * @param {boolean} [mask=false] 是否遮罩(透明层)
+     * @returns 返回 promise 对象
+     */
+    public showCancel(title: string = '', duration: number = 2000, mask: boolean = false) {
+        const image: string = '/assets/toast-icons/toast-cancel.png';
         return this.show({
             title, duration, mask, image
         });
@@ -92,6 +140,6 @@ class Toast {
     }
 }
 
-const toast = new Toast();
+const toast = new Toast(path);
 
 export default toast
