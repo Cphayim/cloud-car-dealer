d61b7f94f5624994ba300dc0796c1b57b70973b8
Cphayim <admin@cphayim.me>
2017-09-18 00:46:00 +0800

    A: 员工提醒

--- src/pages/detail/chat/chat-remind/chat-remind.ts
+++ src/pages/detail/chat/chat-remind/chat-remind.ts
@@ -1,4 +1,9 @@
 import BasePage from '../../../basepage';
+import { request } from '../../../../modules/request';
+import { domain } from '../../../../config/config';
+import { resCodeCheck } from '../../../../modules/auth';
+import toast from '../../../../modules/toast';
+import { modal } from '../../../../modules/modal';
 /*
  * 聊天界面-提醒逻辑
  * @Author: 云程科技 
@@ -8,89 +13,62 @@ import BasePage from '../../../basepage';
  */
 
 class ChatRemindPage extends BasePage {
-  public data: any = {
-    // 列表底部填充层高度
-    paddingLayerHeight: 0,
-    remindList: [{
-      realname: '朱立俊',
-      username: '老村夫',
-      role: '全干(zhàn)架构师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1467.JPG'
-    },
-    {
-      realname: '佳衡',
-      username: 'Cphayim',
-      role: '前端开发工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/cphayim-avatar-2017.jpg'
-    },
-    {
-      realname: '泽凯',
-      username: 'Kyle萧萧',
-      role: 'UI设计师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1465.JPG'
-    },
-    {
-      realname: '叶梓',
-      username: '迎風别葉index',
-      role: '网络运维工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1463.JPG'
-    }, {
-      realname: '右晨',
-      username: '守心',
-      role: 'Java开发工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1464.JPG'
-    },
-    {
-      realname: '文龙',
-      username: '',
-      role: 'iOS开发工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1470.PNG'
-    },
-    {
-      realname: '惠铭',
-      username: '毛头小子‘’',
-      role: '前端开发工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1469.JPG'
-    },
-    {
-      realname: '圣平',
-      username: '奔跑',
-      role: 'PHP开发工程师',
-      avatar: 'http://ofwf89b21.bkt.clouddn.com/avatar/IMG_1466.JPG'
+    private id: number = 0;
+    public data = {
+        loaded: false,
+        employeeList: [],
+        activeIndex: -1,
+        activeEmployeeId: ''
     }
-    ]
-  }
-  // 设置列表视图滚动容器高度
-  setPaddingLayerHeight() {
-    wx.createSelectorQuery().select('.m-remind-btn').boundingClientRect(res => {
-      this.setData({
-        paddingLayerHeight: res.height
-      })
-    }).exec();
-  }
-  onLoad(options) {
-    this.setPaddingLayerHeight();
-  }
-  onReady() {
-
-  }
-  onShow() {
-
-  }
-  onHide() {
-
-  }
-  onUnload() {
-
-  }
-  onPullDownRefresh() {
-
-  }
-  onReachBottom() {
-
-  }
-  onShareAppMessage() {
-
-  }
+    private onLoad(options) {
+        this.id = ~~options.id;
+        this.loadData();
+    }
+    private loadData() {
+        toast.showLoading();
+        request({
+            url: domain + '/WX/Message/ForwardReminder',
+            data: {
+                customerId: this.id
+            }
+        }).then((res: any) => {
+            if (resCodeCheck(res)) { return }
+            this.setData({
+                loaded: true,
+                employeeList: res.data
+            });
+            toast.hide();
+        })
+    }
+    public chooseEmployee(e) {
+        const { employeeId, index } = e.currentTarget.dataset;
+        this.setData({
+            activeIndex: ~~index,
+            activeEmployeeId: employeeId
+        })
+    }
+    public save() {
+        if (!this.data.activeEmployeeId) {
+            return toast.showWarning('必须选择一个员工');
+        }
+        toast.showLoading();
+        request({
+            url: domain + '/WX/Message/Reminder',
+            data: {
+                employeeId: this.data.activeEmployeeId,
+                customerId: this.id
+            }
+        }).then(res => {
+            if (resCodeCheck(res)) { return }
+            toast.hide();
+            modal.show({
+                title: '',
+                content: '已发送提醒',
+                showCancel: false
+            }).then(flag => {
+                wx.navigateBack();
+            })
+        })
+    }
 }
 Page(new ChatRemindPage());
