80380f125d882bae518e2ab927fac98b5482a141
Cphayim <admin@cphayim.me>
2017-09-19 09:20:50 +0800

    fix: 修复 modal 通用模块在安卓机型上取消判断问题

--- src/modules/modal.ts
+++ src/modules/modal.ts
@@ -3,7 +3,7 @@
  * @Author: Cphayim
  * @Date: 2017-08-10 22:25:14 
  * @Last Modified by: Cphayim
- * @Last Modified time: 2017-08-10 22:37:14
+ * @Last Modified time: 2017-09-19 09:16:10
  */
 export const modal = {
     show({
@@ -19,14 +19,22 @@ export const modal = {
             wx.showModal({
                 title, content, showCancel, cancelText, cancelColor, confirmText, confirmColor,
                 success(res) {
+                    /**
+                     * 点击确定按钮        res.confirm 为 true, res.cancel 为 false
+                     * 点击取消按钮        res.confirm 为 false, res.cancel 为 true
+                     * 点击遮罩层(安卓)    res.confirm 为 false, res.cancel 为 false (此时应同取消逻辑)
+                     */
                     if (res.confirm) {
+                        // 用户点击了确定
                         resolve(true);
-                    } else if (res.cancel) {
+                    } else {
+                        // 用户没有点击确定--点了取消或点了遮罩(安卓)
                         resolve(false);
                     }
                 },
                 fail(err) {
-                    console.log(err);
+                    console.error(err);
+                    reject(err);
                 }
             });
         });
