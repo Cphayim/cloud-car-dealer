56a85b2f9415a776731da3c544a78e9bbdbad7fe
Cphayim <admin@cphayim.me>
2017-10-23 10:21:21 +0800

    add: Tab 页，包含 TabBar 组件和各 Tab页 子路由视图

---
+++ web/src/pages/Tab.vue
@@ -0,0 +1,49 @@
+<style lang="scss" scoped>
+@import '../styles/style-config.scss';
+.container {
+  width: 100%;
+  // height: 100%;
+  padding-top: 40px;
+  background-color: $color_back;
+  &.hastab{
+    padding-bottom: 50px;
+  }
+}
+</style>
+
+<template>
+  <div class="container hastab">
+    <!-- Tab 页内容区 -->
+    <div class="wraper">
+      <!-- 子路由 -->
+      <router-view></router-view>
+      <!-- /子路由 -->
+    </div>
+    <!-- /Tab 页内容区 -->
+    <!-- Tab 栏 -->
+    <tab-bar></tab-bar>
+    <!-- /Tab 栏 -->
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/business/TabBar'
+import HomePage from '@/pages/Tabs/Home'
+
+export default {
+  name: 'TabPage',
+  components: { TabBar, HomePage },
+
+  data() {
+    return {
+      activeTab: 'home'
+    }
+  },
+  methods:{
+    changeTab(name){
+      this.activeTab = name
+    }
+  }
+
+}
+</script>
