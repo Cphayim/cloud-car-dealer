db4b4c1701f78b7567f70286f32c805d08d9b9be
Cphayim <admin@cphayim.me>
2017-10-23 10:16:52 +0800

    update: 添加路由视图、样式修改

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,37 @@
-<style lang="scss" scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+@import './styles/style-config.scss';
+html,
+body {
+  // overflow: hidden;
+  height: 100%;
+  margin: 0;
+  background-color: $color_back;
 }
 
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif; // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+}
+*{
+  box-sizing: border-box;
+}
 </style>
 
 <template>
   <div id="app">
-    
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
-  components:{
-  }
+  components: { }
 }
 </script>
 
