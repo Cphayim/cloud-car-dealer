d72a5ef40e31d64a18617ce662d76b87f6ed0f46
Cphayim <admin@cphayim.me>
2017-10-25 10:26:51 +0800

    update: #10.25/10:26

--- web/src/pages/Tabs/Home.vue
+++ web/src/pages/Tabs/Home.vue
@@ -41,10 +41,10 @@
       align-items: center;
       justify-content: flex-end;
       @include font($font_nano, $color_f2);
-      >div {
+      > div {
         // display: inline-block;
         padding-left: r(2);
-        >span {
+        > span {
           @include font($font_nano, $color_em);
         }
       }
@@ -109,7 +109,7 @@
     align-items: center;
     .desc {
       padding-left: r(5);
-      >span {
+      > span {
         display: block;
       }
       .name {
@@ -264,6 +264,7 @@ export default {
   data() {
     return {
       role: '',
+      roleEnum: CONFIG.ROLES,
       pageTitle: '', // 这里之后根据登录角色来更变
       progressCircelConfig: {
         radius: 20,
@@ -374,8 +375,24 @@ export default {
         }
       })
     },
-    loadSales() { },
-    loadService() { },
+    loadSales() {
+      this.$axios({
+        url: CONFIG.HOST + '/Report/Statistic/GetEmployeeTotal?&isNew=false&r=' + Math.random(),
+        method: 'get'
+      }).then(res => {
+        console.log(res)
+        '{"OpenId":"","TenantId":340,"Role":"WX_Sales","Name":"钟-销售顾问","Id":"ed0f77ec-1020-4598-9aa7-c95b38e34426","MyCustomerPre":314,"MyCustomerPreIncreased":7,"MyCustomerPreUnIncreased":6,"MyDeliverCarMonthCount":1,"MyDefeatCount":1,"GroupCustomerPre":0,"GroupCustomerPreIncreased":0,"GroupCustomerPreUnIncreased":336,"GroupDeliverCarCount":0,"GroupDeliverAllCount":0,"GroupDefeatCount":0,"GroupMonthDeliverCarCount":0,"MyCustomerAfter":0,"MyCustomerAfterUnassign":0,"MyCustomerAfterUndeal":0,"TenantCustomerAfter":0,"TenantCustomerAfterUnassign":0,"TenantCustomerAfterUndeal":0,"GroupCustomerAfter":0,"GroupCustomerAfterUndeal":0,"MyBusinessPre":189,"MyBusinessAfter":0,"MyMaintain":0,"GroupMaintain":0,"MyReinsurance":0,"GroupReinsurance":0,"MyBusinessPreUndeal":57,"MyBusinessAfterUndeal":0,"MyMaintainUndeal":0,"GroupMaintainUndeal":0,"MyReinsuranceUndeal":0,"GroupReinsuranceUndeal":0,"GroupBusinessPre":0,"GroupBusinessAfter":0,"GroupBusinessPreUndeal":0,"GroupBusinessAfterUndeal":0,"MyMessage":60,"MyMessageUnreply":4,"GroupMessage":0,"GroupMessageAfter":0,"GroupMessageUnreply":0,"GroupMessageAfterUnreply":0,"MyActiveCustomer":7,"MyActiveCustomerUndeal":80,"GroupActiveCustomer":0,"GroupActiveCustomerUndeal":0,"GroupActiveCustomerAfter":0,"GroupActiveCustomerAfterUndeal":0,"SOSMessageTotal":0,"SOSMessageUndeal":0,"MyVisitTotal":0,"MyNoVisitTotal":59,"GroupNoVisitTotal":0,"MyDeliverCarCount":0,"MyDeliverAllCount":25,"MyBirthdayCount":0,"GroupBirthdayCount":0,"MyNeedMaintain":0,"GroupNeedMaintain":0,"MyNeedReinsurance":0,"GroupNeedReinsurance":0,"MyNeedInspection":0,"GroupNeedInspection":0,"MySaleGoal":6,"MySignGoal":1,"GroupSaleGoal":10,"GroupSignGoal":0,"MyFollowTargetGoal":10,"MyFollowTargetGoalSignGoal":1,"GroupFollowTargetGoal":10,"GroupFollowTargetGoalSignGoal":1,"GroupAfterFollowTargetGoal":0,"MyEvaluationTotal":42,"MyEvaluationSumScore":33,"GroupEvaluationTotal":0,"GroupEvaluationSumScore":0,"GroupVisitTotal":0,"EmployeeMessageTotal":496,"TodayOrderDeal":0,"TodayOrderDealAmount":0.00,"MaintainUndeal":0,"ServiceMessageUnReply":0}'
+      })
+    },
+    loadService() {
+      this.$axios({
+        url: CONFIG.HOST + '/Report/Statistic/GetEmployeeTotal?&isNew=false&r=' + Math.random(),
+        method: 'get'
+      }).then(res => {
+        console.log(res)
+        '{"OpenId":null,"TenantId":340,"Role":"WX_Service","Name":"钟-售后顾问","Id":"7e420c02-fb87-4123-8b4e-266590154281","MyCustomerPre":0,"MyCustomerPreIncreased":0,"MyCustomerPreUnIncreased":0,"MyDeliverCarMonthCount":0,"MyDefeatCount":0,"GroupCustomerPre":0,"GroupCustomerPreIncreased":0,"GroupCustomerPreUnIncreased":0,"GroupDeliverCarCount":0,"GroupDeliverAllCount":0,"GroupDefeatCount":0,"GroupMonthDeliverCarCount":0,"MyCustomerAfter":155,"MyCustomerAfterUnassign":1,"MyCustomerAfterUndeal":117,"TenantCustomerAfter":482,"TenantCustomerAfterUnassign":199,"TenantCustomerAfterUndeal":183,"GroupCustomerAfter":0,"GroupCustomerAfterUndeal":0,"MyBusinessPre":0,"MyBusinessAfter":36,"MyMaintain":55,"GroupMaintain":0,"MyReinsurance":0,"GroupReinsurance":0,"MyBusinessPreUndeal":0,"MyBusinessAfterUndeal":15,"MyMaintainUndeal":3,"GroupMaintainUndeal":0,"MyReinsuranceUndeal":0,"GroupReinsuranceUndeal":0,"GroupBusinessPre":0,"GroupBusinessAfter":0,"GroupBusinessPreUndeal":0,"GroupBusinessAfterUndeal":0,"MyMessage":31,"MyMessageUnreply":3,"GroupMessage":0,"GroupMessageAfter":0,"GroupMessageUnreply":0,"GroupMessageAfterUnreply":0,"MyActiveCustomer":8,"MyActiveCustomerUndeal":35,"GroupActiveCustomer":0,"GroupActiveCustomerUndeal":0,"GroupActiveCustomerAfter":0,"GroupActiveCustomerAfterUndeal":0,"SOSMessageTotal":22,"SOSMessageUndeal":7,"MyVisitTotal":0,"MyNoVisitTotal":0,"GroupNoVisitTotal":0,"MyDeliverCarCount":0,"MyDeliverAllCount":0,"MyBirthdayCount":3,"GroupBirthdayCount":0,"MyNeedMaintain":0,"GroupNeedMaintain":0,"MyNeedReinsurance":2,"GroupNeedReinsurance":0,"MyNeedInspection":1,"GroupNeedInspection":0,"MySaleGoal":0,"MySignGoal":0,"GroupSaleGoal":0,"GroupSignGoal":0,"MyFollowTargetGoal":2,"MyFollowTargetGoalSignGoal":0,"GroupFollowTargetGoal":0,"GroupFollowTargetGoalSignGoal":0,"GroupAfterFollowTargetGoal":4,"MyEvaluationTotal":12,"MyEvaluationSumScore":6,"GroupEvaluationTotal":0,"GroupEvaluationSumScore":0,"GroupVisitTotal":0,"EmployeeMessageTotal":65,"TodayOrderDeal":0,"TodayOrderDealAmount":0.00,"MaintainUndeal":0,"ServiceMessageUnReply":0}'
+      })
+     },
 
     loadShare(){
       this.$axios({
@@ -390,22 +407,29 @@ export default {
     }
   },
   created() {
+
+    // 判断后端是否传递了 GLOBAL_ROLE
     if (typeof GLOBAL_ROLE !== 'string') return this.$toast('获取角色信息失败!')
-    this.role = GLOBAL_ROLE.slice(3)
+
+    this.role = GLOBAL_ROLE
 
     console.log(this.role)
+
+    // 加载角色所属数据
     switch (this.role) {
-      case 'Sales':
-        this.loadScales(); this.pageTitle = '销售顾问'; break
-      case 'Service':
-        this.loadService(); this.pageTitle = '售后顾问'; break
-      case 'Admin':
-        this.loadAdmin(); this.pageTitle = '店铺管理员'; break
+      case this.roleEnum['销售顾问']:
+        this.loadSales();  break
+      case this.roleEnum['售后顾问']:
+        this.loadService(); break
+      case this.roleEnum['店铺管理员']:
+        this.loadAdmin();  break
       default:
         this.$toast('角色信息有误!')
         return
     }
+    this.pageTitle = this.roleEnum[this.role] || '未知角色'
 
+    // 加载朋友圈营销 lastMsg
     this.loadShare()
   }
 }
